f33db3ca8458651ef90178b964cea3b98e7e6ed7
Nathanael Williams <nathanaelwilliams93@gmail.com>
2021-03-24 13:29:25 -0400

    Some Frontend work

--- project2frontend/src/App.js
+++ project2frontend/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+          
+          </Route>
+
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
