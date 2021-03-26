e7629a941be5d322d41f6cfec61881f5d66e0cd5
Lindsay Read <lindsayread1995@gmail.com>
2021-03-26 07:04:55 -0600

    added store component and add new item component. Also fixed merging conflicts

--- project2frontend/src/App.js
+++ project2frontend/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -46,7 +23,6 @@ function App() {
         </Switch>
       </div>
     </Router>
->>>>>>> Nathanael
   );
 }
 
---
+++ project2frontend/src/Store.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Logo from './logo.jpg';
+// function ShowItems() {
+const ShowItems = (props)=>{
+    return (
+        <div className="store_catalogue">
+            <Link to='./store_catalogue'>
+            </Link>
+            <div className='store_container'>
+                <table>
+                    <tr>
+                        <th>Image of Product</th>
+                        <th>Name of Product</th>
+                        <th>Price</th>
+                        <th>Add to Cart(with quantity)</th>
+                        <th>Quantity</th>
+                    </tr>
+                    {/* //for each item, add the row:
+                    React.createElement("tr",null,null);
+                    React.createElement("td",img,)
+                    React.createElement("td",name,)
+                    React.createElement("td",description,)
+                    React.createElement("td",price,)
+                    React.createElement("td",quantity,) */}
+                    {/* <tr>
+                        <td>{props.img}</td>
+                        <td>{props.name}</td>
+                        <td>{props.description}</td>
+                        <td>{props.price}</td>
+                        <td>**counter**</td>
+                    </tr> */}
+                </table>
+            </div>
+            <div>
+                <Link to="/add_new_item" >
+                    <button className='login__registerButton'>Add New Item</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+export default ShowItems();
