556bc7b91505f30413653abb37c09f592aabd91c
ReyA105 <33044468+ReyA105@users.noreply.github.com>
2021-03-30 20:57:30 -0500

    Update OrderServices.java

--- Project2Backend/src/main/java/com/revature/services/OrderServices.java
+++ Project2Backend/src/main/java/com/revature/services/OrderServices.java
@@ -1,5 +1,49 @@
 package com.revature.services;
 
+import com.revature.dao.OrderDao;
+import com.revature.model.Order;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor=@__(@Autowired))
 public class OrderServices {
+	private OrderDao orderDao;
+
+	public Order getOrderById(int order_id) {
+		Order order =  orderDao.findById(order_id);
+		if(order==null) {
+			return null;
+		}
+		return order;
+	}
+
+	public List<Order> getAllOrders() {
+		return orderDao.findAll();
+	}
+
+	public List<Order> getAllCreatorOrders(int creator_id) {
+		return orderDao.findByCreator(creator_id);
+	}
+
+	public void insertOrder(Order order) {
+			orderDao.save(order);
+	}
+
+	public void deleteOrder(Order order) {
+			orderDao.delete(order);
+	}
+
+	public void updateOrder(Order order) {
+		
+	}
+	
 
 }
