63d54a35596a27398e77a1732287ee9051d0cecd
Rey <reynaldoamaya99@gmail.com>
2021-04-01 21:03:38 -0500

    updating backend

--- Project2Backend/src/main/java/com/revature/controller/UserController.java
+++ Project2Backend/src/main/java/com/revature/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.revature.controller;
 
+import java.security.SecureRandom;
 import java.util.List;
 
+import com.revature.repository.UserDao;
 import com.revature.model.User;
 import com.revature.services.UserServices;
 
@@ -10,6 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
+/*
+* User Controller!
+*/
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
@@ -17,19 +24,28 @@ public class UserController {
 
 	@Autowired
 	private UserServices userServices;
+	@Autowired
+	private UserDao userDao;
 
-	// Get all Users
+	/*
+	* Get all Users
+	*/
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		return this.userServices.getAll();
 	}
 
-	// Create User rest api
+	/*
+	* Create the User 
+	*/
 	@PostMapping()
 	public User createUser(@RequestBody User user) {
 		return this.userServices.insertUser(user);
 	}
 
+	/*
+	* Get the User by email and password to login
+	*/
 	@GetMapping("/user")
 	public ResponseEntity<?> getUserByLogin(@RequestParam String emailId, @RequestParam String password) {
 		User user = this.userServices.findByEmailAndPass(emailId, password);
@@ -39,7 +55,9 @@ public class UserController {
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 
-	// Get User by id rest api
+	/*
+	* Get the User by id
+	*/
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable int id) {
 		if (this.userServices.getUserById(id) == null) {
@@ -48,12 +66,69 @@ public class UserController {
 		return new ResponseEntity<>(this.userServices.getUserById(id), HttpStatus.OK);
 	}
 
-	// Delete User from api
+	/*
+	* Find the User by email
+	*/
+	@GetMapping("/reset/{email}")
+	public void findUserByEmail(@PathVariable String email) {
+		User user = this.userServices.getUserByEmail(email);
+		if (user == null) {
+			 System.out.println("User is null");
+		}
+		//Replace it with your gmail creds.
+		String from = "revatureshop"; // GMail user name (just the part before "@gmail.com")
+        String pass = "p4ssword123"; // GMail password
+		String[] to = { user.getEmail() };
+
+		int passwordLength = 5;
+		String newPassword = generateRandomPassword(passwordLength);
+        System.out.println(newPassword);
+
+        String subject = "Revature Shop: Password";
+        String body = "Your new password is " + newPassword +
+					 ". You will be able to change your password in the Revature Shop app.";
+		user.setPassword(newPassword);
+        userServices.sendFromGMail(from, pass, to, subject, body);
+		
+	}
+
+	/*
+	*Update the User's Password
+	*/
+	@PutMapping("/user/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User userPassword){
+		User user = this.userServices.getUserById(id);
+		user.setPassword(userPassword.getPassword());		
+		User updatedUser = userDao.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+
+	/*
+	*Delete User from api
+	*/
 	@DeleteMapping("/users/{id}")
 	public ResponseEntity<String> deleteUser(@PathVariable("id") int id) {
 		User user = this.userServices.getUserById(id);
 		this.userServices.deleteUser(user);
 		return new ResponseEntity<>("Resource Deleted", HttpStatus.GONE);
 	}
+
+	 /*
+	 * Generates Random Password
+	 */
+	 public static String generateRandomPassword(int len){
+		 // ASCII range - alphanumeric (0-9, a-z, A-Z)
+		 final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		 SecureRandom random = new SecureRandom();
+		 StringBuilder sb = new StringBuilder();
+		 // each iteration of loop choose a character randomly from the given ASCII range
+		 // and append it to StringBuilder instance
+		 for (int i = 0; i < len; i++) {
+			 int randomIndex = random.nextInt(chars.length());
+			 sb.append(chars.charAt(randomIndex));
+		 }
+		 return sb.toString();
+	 }
+	
 
 }
--- Project2Backend/src/main/java/com/revature/services/UserServices.java
+++ Project2Backend/src/main/java/com/revature/services/UserServices.java
@@ -1,42 +1,113 @@
-
 package com.revature.services;
 
 import java.util.List;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
-
-import com.revature.model.User;
 import com.revature.repository.UserDao;
+import com.revature.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import lombok.NoArgsConstructor;
 
+/*
+* UserServices!
+*/
 @Service
 @NoArgsConstructor
 public class UserServices {
 
-    private UserDao userDao;
+	@Autowired
+	private UserDao userDao;
 
-    public User findByEmailAndPass(String email, String password) {
-        User user = userDao.findByEmailAndPassword(email, password);
-        return user;
-    }
+	/*
+	 * Find by the email and password
+	 */
+	public User findByEmailAndPass(String email, String password) {
+		User user = userDao.findByEmailAndPassword(email, password);
+		return user;
+	}
 
-    public List<User> getAll() {
-        return userDao.findAll();
-    }
+	/*
+	 * Get all users
+	 */
+	public List<User> getAll() {
+		return userDao.findAll();
+	}
 
-    public User getUserById(int id) {
-        User user = userDao.findUserByUserId(id);
-        if (user == null) {
-            return null;
-        }
-        return user;
-    }
+	/*
+	 * Get user by id
+	 */
+	public User getUserById(int id) {
+		User user = userDao.findUserById(id);
+		if (user == null) {
+			return null;
+		}
+		return user;
+	}
 
-    public User insertUser(User user) {
-        return userDao.save(user);
-    }
+	/*
+	 * Get the email from the current user
+	 */
+	public User getUserByEmail(String email) {
+		User user = userDao.findUserByEmail(email);
+		if (user == null) {
+			return null;
+		}
+		return user;
+	}
 
-    public void deleteUser(User user) {
-        userDao.delete(user);
-    }
+	/*
+	 * Insert the user
+	 */
+	public User insertUser(User user) {
+		return userDao.save(user);
+	}
+
+	/*
+	 * Delete the user
+	 */
+	public void deleteUser(User user) {
+		userDao.delete(user);
+	}
+
+	/*
+	 * Send email from gmail using the Revatureshop email I created
+	 */
+	public void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+		Properties props = System.getProperties();
+		String host = "smtp.gmail.com";
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.user", from);
+		props.put("mail.smtp.password", pass);
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage message = new MimeMessage(session);
+
+		try {
+			message.setFrom(new InternetAddress(from));
+			InternetAddress[] toAddress = new InternetAddress[to.length];
+			for (int i = 0; i < to.length; i++) {
+				toAddress[i] = new InternetAddress(to[i]);
+			}
+			for (int i = 0; i < toAddress.length; i++) {
+				message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+			}
+			message.setSubject(subject);
+			message.setText(body);
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (AddressException ae) {
+			ae.printStackTrace();
+		} catch (MessagingException me) {
+			me.printStackTrace();
+		}
+	}
 }
