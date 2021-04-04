93ea90ea22365e9f45795466cef566a792be3ecd
Rey <reynaldoamaya99@gmail.com>
2021-04-04 13:34:59 -0500

    adding cart files to backend

---
+++ Project2Backend/src/main/java/com/revature/controller/CartController.java
@@ -0,0 +1,53 @@
+package com.revature.controller;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.model.Cart;
+import com.revature.model.Item;
+import com.revature.model.User;
+import com.revature.services.CartServices;
+import com.revature.services.UserServices;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/cart")
+@AllArgsConstructor(onConstructor=@__(@Autowired))
+@NoArgsConstructor
+public class CartController {
+
+	private CartServices cartServ;
+	private UserServices uServ;
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<?> getCartByUser(@PathVariable("userId")String userId){
+		return new ResponseEntity<>(cartServ.findByCartCreator(uServ.getUserById(Integer.parseInt(userId))), HttpStatus.OK);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Cart> addCartToUser(@RequestBody LinkedHashMap<String, Object> cMap) {
+		int userId = (Integer) cMap.get("userId");
+		User cUser = uServ.getUserById(userId);
+		System.out.println(cUser.getUsername());
+		Cart c = cartServ.findByCartCreator(cUser);
+		if(c == null) {
+			c = new Cart(new ArrayList<Item>(), 0, cUser);
+			cartServ.insertCart(c);
+			return new  ResponseEntity<>(c, HttpStatus.OK);
+		}
+		cartServ.insertCart(c);
+		return new  ResponseEntity<>(c, HttpStatus.OK);
+	}
+	
+}
--- Project2Backend/src/main/java/com/revature/model/Cart.java
+++ Project2Backend/src/main/java/com/revature/model/Cart.java
@@ -9,9 +9,12 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,16 +33,21 @@ public class Cart {
 	@Id
 	@Column(name="cart_id")
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int cartNumber;
-	
-	@OneToOne(mappedBy="currentCart")
-	private User cartCreator;
-	
+	private int cartId;
 	@ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	private List<Item> items;
-	
 	@Column(name="cart_total")
 	private double amount;
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "orders", "userCart"})
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	@JoinColumn(name="creator_fk")
+	private User cartCreator;
 	
+	public Cart(List<Item> items, double amount, User cartCreator) {
+		super();
+		this.items = items;
+		this.amount = amount;
+		this.cartCreator = cartCreator;
+	}
 	
 }
