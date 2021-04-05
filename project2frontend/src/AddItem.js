b3a769a1f6b0e352c2e0e71afa13befd717fcddf
lindsayread <65792127+lindsayread@users.noreply.github.com>
2021-04-05 08:53:46 -0600

    removing comments

--- project2frontend/src/AddItem.js
+++ project2frontend/src/AddItem.js
@@ -7,26 +7,17 @@ import ItemServices from './services/ItemService';
 {/* NewItem! */}
 function NewItem(props) {
     let [item, setItem] = useState({name:null, photo:null, price:null})
-    //let formData = new FormData()
-    //formData.append('imageUpload',imageUpload)
-    //formData.append('fName',fName)
-
-    // funtion handleChange(e){
-    //     let val = e.target.value;
-    //     console.log(val)
-    //     console.log(e.target.name)
-    // }
+    
     const onChange = (e) => {
-        //formData.append('imageUpload',item.photo);
+        
         setItem({ ...item,[e.target.name]: e.target.value}) 
         
     }
     const addimage = (e) => {
         let formData = new FormData();
-        //formData.append('imageUpload',item.photo);
+        
         console.log(formData);
-        //formData.append('imageUpload',item.photo);
-        // setItem({ ...item,["item.photo"]: formData.append('imageUpload',item.photo)});
+
         setItem({ ...item,["item.photo"]: formData.append('imageUpload',item.photo)});
         console.log(formData);
     }
@@ -34,8 +25,7 @@ function NewItem(props) {
     function submitItem(e) {
         //add item from state with itemservice
         console.log(item);
-        //formData.append('imageUpload',item.photo);
-        //formData.append(item.photo,'imageUpload',)
+        
         e.preventDefault();
         ItemServices.createItem(item)
     }
@@ -56,21 +46,12 @@ function NewItem(props) {
                             {/* <button type="submit">Upload Image</button> */}
                         </form>
                     </div>
-                    {/* <h5>Item Image</h5>
-                    <input type='text' /> */}
 
                     <h5>Name of Item</h5>
                     <input name="name" value={item.name} onChange={onChange} type='text' />
 
                     <h5>Price of Item</h5>
                     <input name="price" value={item.price} onChange={onChange} type='number' />
-
-                    {/* <h5>Type of Item</h5>
-                    <select className="dropdown">
-                        <option value="Electronics">Electronics</option>
-                        <option value="Food">Food</option>
-                        <option value="Clothing">Clothing</option>
-                    </select> */}
 
                     <button type='submit' className='login__signInButton' onClick={submitItem} >Submit New Item to Store</button>
 
